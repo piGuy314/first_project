83f3d22529c80470e809c698984d533b753bbf0d
Aaron Eichhorst <aaroneichhorst@gmail.com>
2017-05-13 15:56:22 +0000

    Updated contact_page errors

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,9 @@
 class ContactsController < ApplicationController
-
-  
-
   # GET request to /contact-us
   # Show new contact form
-
   def new
     @contact = Contact.new    
   end
-  
-
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
@@ -20,13 +14,12 @@ class ContactsController < ApplicationController
       flash[:success] = "Message sent."
       redirect_to new_contact_path
     else
-
   # POST request /contacts
   def create
     # Mass assignment of form fields into Contact object
     @contact = Contact.new(contact_params)
     # Save the Contact object to the database
-    if @contact.save
+  if @contact.save
       # Store form fields via parameters, into variables
       name = params[:contact][:name]
       email = params[:contact][:email]
@@ -38,22 +31,14 @@ class ContactsController < ApplicationController
       # and redirect to the new action
       flash[:success] = "Message sent."
       redirect_to new_contact_path
-    else
+  else
       # If Contact object doesn't save
       # store errors in flash hash, 
       # and redirect to the new action
-
       flash[:danger] = @contact.errors.full_messages.join(", ")
       redirect_to new_contact_path
-    end
   end
-  
   private
-
-
-  # To collect data from form, we need to use
-  # strong parameters and whitelist the form fields
-
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
     end
