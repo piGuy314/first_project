7beb24897cc3559d5d7c98585ed83b1d2c94f334
Aaron Eichhorst <aaroneichhorst@gmail.com>
2017-05-18 20:09:39 +0000

    Update master branch

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,54 +1,42 @@
+
 class ContactsController < ApplicationController
+
   # GET request to /contact-us
   # Show new contact form
   def new
-    @contact = Contact.new    
+    @contact = Contact.new
   end
-<<<<<<< HEAD
-  
-=======
-  def create
-    @contact = Contact.new(contact_params)
-    if @contact.save
-      name = params[:contact][:name]
-      email = params[:contact][:email]
-      body = params[:contact][:comments]
-      ContactMailer.contact_email(name, email, body).deliver
-      flash[:success] = "Message sent."
-      redirect_to new_contact_path
-    else
->>>>>>> contact_form
-  # POST request /contacts
+
+  # POST request to /contacts
   def create
     # Mass assignment of form fields into Contact object
     @contact = Contact.new(contact_params)
-    # Save the Contact object to the database
-  if @contact.save
-      # Store form fields via parameters, into variables
+    # Save the contact object to database
+    if @contact.save
+      # store form fields via parameters, into variables
       name = params[:contact][:name]
       email = params[:contact][:email]
       body = params[:contact][:comments]
-      # Plug variables into Contact mailer 
+      # Plug variables into Contact Mailer
       # email method and send email
-       ContactMailer.contact_email(name, email, body).deliver
-      # Store success message in flash hash 
-      # and redirect to the new action
-      flash[:success] = "Message sent."
+      ContactMailer.contact_email(name, email, body).deliver
+      # Store success message in flash hash
+      # redirect to new action
+      flash[:success] = "Message Sent."
       redirect_to new_contact_path
-  else
-      # If Contact object doesn't save
-      # store errors in flash hash, 
-      # and redirect to the new action
+    else
+      # if contact object doesn't save,
+      # store error in flash hash,
+      # and redirect_to the new action
       flash[:danger] = @contact.errors.full_messages.join(", ")
       redirect_to new_contact_path
-  end
-  private
-<<<<<<< HEAD
-  # To collect data from form, we need to use
-  # strong parameters and whitelist the form fields
-=======
->>>>>>> contact_form
-    def contact_params
-      params.require(:contact).permit(:name, :email, :comments)
     end
+  end
+
+  private
+  # collect data from form we need to use strong
+  # parameters and whitelist the form fields 
+  def contact_params
+    params.require(:contact).permit(:name, :email, :comments)
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.sntp_setting = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'agile-cliffs-90376.herokuapp.com',
-    :authentication => :plain,
-    
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'shrouded-fortress-66409.herokuapp.com',
+  :authentication => :plain,
 }
-ActionMailer::Base.delivery_method = :sntp
+ActionMailer::Base.delivery_method = :smtp
