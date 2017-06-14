24111e1dd9474dc050ee2036c4e18e9de5f34d57
Aaron Eichhorst <aaroneichhorst@gmail.com>
2017-06-14 00:59:28 +0000

    ran db migrate

--- db/migrate/20170505025151_create_contacts.rb
+++ db/migrate/20170505025151_create_contacts.rb
@@ -1,19 +1,4 @@
 class CreateContacts < ActiveRecord::Migration[5.0]
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> contact_form
-  def change
-    create_table :contacts do |t|
-      t.string   :name
-      t.string   :email
-      t.text     :comments
-      t.timestamps
-    end
-  end
-end
-<<<<<<< HEAD
-=======
     def change
     create_table :contacts do |t|
      t.string  :name
@@ -23,6 +8,3 @@ end
     end
     end
 end
->>>>>>> 531ec77ee58a81470d75239d19179df5ac43f7aa
-=======
->>>>>>> contact_form
